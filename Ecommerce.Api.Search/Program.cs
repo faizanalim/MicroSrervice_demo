af013bffd3884c489c62141eb34df0b750571987
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2024-07-28 05:44:42 +0500

    Implement search service and invoke orders microservice from search

--- Ecommerce.Api.Search/Program.cs
+++ Ecommerce.Api.Search/Program.cs
@@ -1,3 +1,18 @@
+using Ecommerce.Api.Search.Interfaces;
+using Ecommerce.Api.Search.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +21,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ISearchService, SearchService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+builder.Services.AddHttpClient("OrdersService", config =>
+{
+	config.BaseAddress = new Uri(configuration["Services:Orders"]);
+});
 
 var app = builder.Build();
 
---
+++ Ecommerce.Api.Search/Services/SearchService.cs
@@ -0,0 +1,40 @@
+﻿using Ecommerce.Api.Search.Interfaces;
+
+namespace Ecommerce.Api.Search.Services
+{
+	public class SearchService : ISearchService
+	{
+		private readonly IOrdersService ordersService;
+		
+
+		public SearchService(IOrdersService ordersService )
+		{
+			this.ordersService = ordersService;
+			
+		}
+		public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
+		{
+			var ordersResult = await ordersService.GetOrdersAsync(customerId);
+			
+
+			if (ordersResult.IsSuccess)
+			{
+				//foreach (var order in ordersResult.Orders)
+				//{
+				//	foreach (var item in order.Items)
+				//	{
+				//		item.ProductName = productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name;
+				//	}
+				//}
+
+
+				var result = new
+				{
+					Orders = ordersResult.Orders
+				};
+				return (true, result);
+			}
+			return (false, null);
+		}
+	}
+}
